0dba51817043e14d35ea30d66271f561a785415a
Dylan Pokun <dylan.pokun@kleegroup.com>
2020-11-13 09:58:02 +0100

    Mécanisme d'envoi de POST et GET ok

--- src/main/java/dap/PosteurJSON/App.java
+++ src/main/java/dap/PosteurJSON/App.java
@@ -1,57 +1,110 @@
 package dap.PosteurJSON;
 
-import java.io.FileNotFoundException;
+import java.io.ByteArrayOutputStream;
 import java.io.FileReader;
-import java.io.IOException;
- 
+import java.io.ObjectOutputStream;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
- 
-public class App 
-{
-    @SuppressWarnings("unchecked")
-    public static void main(String[] args) 
-    {
-        //JSON parser object to parse read file
-        JSONParser jsonParser = new JSONParser();
-         
-        try (FileReader reader = new FileReader("conf.json"))
-        {
-            //Read JSON file
-            Object obj = jsonParser.parse(reader);
- 
-            JSONArray employeeList = (JSONArray) obj;
-            System.out.println(employeeList);
-             
-            //Iterate over employee array
-            employeeList.forEach( emp -> parseEmployeeObject( (JSONObject) emp ) );
- 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-    }
- 
-    private static void parseEmployeeObject(JSONObject employee) 
-    {
-        //Get employee object within list
-        JSONObject employeeObject = (JSONObject) employee.get("employee");
-         
-        //Get employee first name
-        String firstName = (String) employeeObject.get("firstName");    
-        System.out.println(firstName);
-         
-        //Get employee last name
-        String lastName = (String) employeeObject.get("lastName");  
-        System.out.println(lastName);
-         
-        //Get employee website name
-        String website = (String) employeeObject.get("website");    
-        System.out.println(website);
-    }
+
+public class App {
+	public static void main(String[] args) {
+		// JSON parser object to parse read file
+		JSONParser jsonParser = new JSONParser();
+
+		try (FileReader reader = new FileReader("sendMe.json")) {
+			// Read JSON file
+			Object obj = jsonParser.parse(reader);
+
+			// Parse Array to JSON
+			JSONArray jsonArray = (JSONArray) obj;
+			// System.out.println(jsonArray);
+
+			// Iterate over json array
+			for (Object jsonObject : jsonArray) {
+				try {
+					// Initial casting
+					JSONObject currentJson = (JSONObject) jsonObject;
+
+					// Get "Headers" object
+					JSONObject headersJson = (JSONObject) currentJson.get("Headers");
+					// System.out.println(headersJson);
+
+					// Get "Headers"'s content
+					String accept = (String) headersJson.get("Accept");
+					String contentType = (String) headersJson.get("Content-Type");
+					String authorization = (String) headersJson.get("Authorization");
+
+					// Get other conf
+					String url = (String) currentJson.get("URL");
+					// System.out.println(url);
+					String method = (String) currentJson.get("Method");
+					// System.out.println(method);
+
+					// Get Body
+					JSONObject bodyJson = (JSONObject) currentJson.get("Body");
+					// System.out.println(headersJson);
+
+					// HTTP-Post
+					CloseableHttpClient httpclient = HttpClients.createDefault();
+					// Init
+					if (method.trim().toLowerCase().equals("post")) {
+						HttpPost post = new HttpPost(url);
+
+						// Headers
+						post.setHeader("Content-Type", contentType);
+						post.setHeader("Accept", accept);
+						post.setHeader("Authorization", authorization);
+
+						// Body
+						String jsonRequestBody = bodyJson.toJSONString();
+						HttpEntity entity = new ByteArrayEntity(jsonRequestBody.getBytes("UTF-8"));
+						post.setEntity(entity);
+
+						// Exec
+						CloseableHttpResponse response = httpclient.execute(post);
+						System.out.println(response.getStatusLine());
+						
+						// Closing
+						response.close();
+					} else if (method.trim().toLowerCase().equals("get")) {
+						HttpGet get = new HttpGet(url);
+						// Headers
+						get.setHeader("Content-Type", contentType);
+						get.setHeader("Accept", accept);
+						get.setHeader("Authorization", authorization);
+						// Exec
+						CloseableHttpResponse response = httpclient.execute(get);
+
+						// Output
+						System.out.println(response.getStatusLine());
+						HttpEntity entity = response.getEntity();
+						String responseBody = EntityUtils.toString(entity, "UTF-8");
+						System.out.println(responseBody.toString());
+
+						// Closing
+						response.close();
+					}
+
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+					// Interruption du traitement
+					System.out.println(HttpStatus.SC_METHOD_FAILURE);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
